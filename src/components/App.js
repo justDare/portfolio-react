f1be47ab71a622bb8a762b78ff68353b2bdd51b7
Darian Sampare <dsampare04@gmail.com>
2019-08-03 14:04:12 -0700

    first commit, stucture and header component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './landing/header/Header';
+import './App.scss';
+
+const App = () => {
+    return (
+        <div id="main">
+            <Header />
+        </div>
+    );
+}
+
+export default App;
