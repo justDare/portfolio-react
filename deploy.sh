10118426fb4f1415a23343466dbf76ba53b594f5
Darian Sampare <dsampare04@gmail.com>
2019-08-17 19:45:50 -0700

    testing deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,11 @@ git checkout master
 # add all added/modified files
 git add .
 # commit changes
+echo Enter Commit Message...
 read commitMessage
 git commit -am "$commitMessage"
 # push to git remote repository
 git push
 ###########################
-echo Press Enter...
+echo Press Enter to Push Changes...
 read
