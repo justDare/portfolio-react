232255f6dfa37ebee78574318d28237202de6330
Darian Sampare <dsampare04@gmail.com>
2020-02-05 12:20:29 -0800

    add tech job me

--- src/components/landing/portfolio/Portfolio.js
+++ src/components/landing/portfolio/Portfolio.js
@@ -1,11 +1,11 @@
-import React from "react";
-import Project from "./Project";
-import "./Portfolio.scss";
-import OnVisible, { setDefaultProps } from "react-on-visible";
+import React from 'react';
+import Project from './Project';
+import './Portfolio.scss';
+import OnVisible, { setDefaultProps } from 'react-on-visible';
 
 setDefaultProps({
   bounce: false,
-  visibleClassName: "visible",
+  visibleClassName: 'visible',
   percent: 10
 });
 
@@ -13,39 +13,46 @@ class Portfolio extends React.Component {
   constructor(props) {
     super(props);
     this.state = { visible: false };
-    this.categories = ["HTML+CSS", "React", "JavaScript", "Python", "CMS"];
+    this.categories = ['HTML+CSS', 'React', 'JavaScript', 'Python', 'CMS'];
     this.projects = [
       {
-        name: "DareToProgram",
-        url: "https://www.daretoprogram.ca/",
-        summary:
-          "My personal blog built with Django. Test it out yourself by signing up and interacting with the posts!",
-        category: "Python",
-        class: "daretoprogram"
+        name: 'TechJobMe',
+        url: 'https://damp-thicket-76216.herokuapp.com/',
+        summary: 'A job appplication tracking tool built with the MERN stack.',
+        category: 'JavaScript',
+        class: 'tech-job-me'
       },
       {
-        name: "Saerch Algorithm Visualizer",
-        url: "https://justdare.github.io/Search-Algorithm-Visualizer/",
+        name: 'DareToProgram',
+        url: 'https://www.daretoprogram.ca/',
         summary:
-          "A visualization tool for common search algorithms bulit in ReactJS.",
-        category: "React",
-        class: "search-algo-visualizer"
+          'My personal blog built with Django. Test it out yourself by signing up and interacting with the posts!',
+        category: 'Python',
+        class: 'daretoprogram'
       },
       {
-        name: "Animikii Social Impact Report",
-        url: "http://2018.animikii.com/",
+        name: 'Saerch Algorithm Visualizer',
+        url: 'https://justdare.github.io/Search-Algorithm-Visualizer/',
         summary:
-          "The 2018 Animikii impact report, built with JS, SCSS, and Yarn.",
-        category: "JavaScript / SCSS",
-        class: "impact"
+          'A visualization tool for common search algorithms bulit in ReactJS.',
+        category: 'React',
+        class: 'search-algo-visualizer'
       },
       {
-        name: "Gathering Our Voices",
-        url: "https://gatheringourvoices.ca/",
+        name: 'Animikii Social Impact Report',
+        url: 'http://2018.animikii.com/',
         summary:
-          "This site was built on WordPress at request of the customer. GOV is currently being maintained by UrsaSol. ",
-        category: "CMS",
-        class: "gov"
+          'The 2018 Animikii impact report, built with JS, SCSS, and Yarn.',
+        category: 'JavaScript / SCSS',
+        class: 'impact'
+      },
+      {
+        name: 'Gathering Our Voices',
+        url: 'https://gatheringourvoices.ca/',
+        summary:
+          'This site was built on WordPress at request of the customer. GOV is currently being maintained by UrsaSol. ',
+        category: 'CMS',
+        class: 'gov'
       }
     ];
   }
@@ -66,7 +73,7 @@ class Portfolio extends React.Component {
 
   render() {
     let rev;
-    if (this.state.visible) rev = "rev-block";
+    if (this.state.visible) rev = 'rev-block';
 
     return (
       <OnVisible id="portfolio" onChange={this.addKeyframe}>
@@ -74,7 +81,7 @@ class Portfolio extends React.Component {
           <h1 className={rev}>
             <span>Portfolio</span>
           </h1>
-          <h1 className={"rev-second " + rev}>
+          <h1 className={'rev-second ' + rev}>
             <span>- Recent Works -</span>
           </h1>
           <div className="portfolioContainer row container mx-auto">
